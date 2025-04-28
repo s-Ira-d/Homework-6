289bd593f2e09607e41ce569573de03a0f6078cb
s-Ira-d <20irinka12@gmail.com>
2025-04-28 13:10:04 +0300

    Update cycles.js

--- js/cycles.js
+++ js/cycles.js
@@ -13,3 +13,11 @@ for (let i = 2; i <= 20; i += 1) {
 for (let seven = 1; seven <= 10; seven += 1) {
   console.log(`7 x ${seven} = ${7 * seven}`);
 }
+let n = 7;
+for (let q = 1; q < 100; q += 1) {
+  if (q >= n) {
+    break;
+  } else {
+    console.log(q);
+  }
+}
