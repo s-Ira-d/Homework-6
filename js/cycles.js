54cfed838a8c1d878c3419cea402d473f105f740
s-Ira-d <20irinka12@gmail.com>
2025-04-28 10:17:29 +0300

    Update cycles.js

--- js/cycles.js
+++ js/cycles.js
@@ -1,5 +1,12 @@
-let i = 1;
-while (i <= 10) {
-  console.log(i);
-  i++;
+let number = 1;
+while (number <= 10) {
+  console.log(number);
+  number += 1;
+}
+for (let i = 2; i <= 20; i += 1) {
+  if (i % 2 !== 0) {
+    continue;
+  } else {
+    console.log(i);
+  }
 }
